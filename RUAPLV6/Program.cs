5e1eabc40695a7c6acef615e567ee503f635d54a
Bruno Zahirović <bruno.zahirovic@etfos.hr>
2021-12-21 12:57:03 +0100

    Inicijalni commit

---
+++ RUAPLV6/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RUAPLV6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
